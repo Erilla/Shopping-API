c60e33c84c39ff8b176afc01674cab8f4832e599
Ryan Wong <me@ryanwong.uk>
2022-05-28 00:19:04 +0100

    Added Business project and setup dependency injection

---
+++ ShoppingAPI.Business/Services/IProductService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoppingAPI.Business.Services
+{
+    internal interface IProductService
+    {
+    }
+}
---
+++ ShoppingAPI.Business/ServicesConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using ShoppingAPI.Business.Repositories;
+using ShoppingAPI.Business.Services;
+
+namespace ShoppingAPI.Business
+{
+    public static class ServicesConfiguration
+    {
+        public static void AddBusinessServices(this IServiceCollection services)
+        {
+            // Repositories
+            services.AddTransient<IProductRepository, ProductRepository>();
+
+            // Services
+            services.AddTransient<IProductService, ProductService>();
+        }
+    }
+}
